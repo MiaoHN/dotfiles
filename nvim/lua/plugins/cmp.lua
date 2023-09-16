64a56f5ef8ca34e0127520dd6d42dfa93155dbb1
MiaoHN <582418227@qq.com>
2023-09-16 09:53:14 +0800

    nvim: remove unused keyword.lua

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -18,7 +18,6 @@ return {
         return
       end
       local lspkind = require("lspkind")
-      require("keyword").setup()
 
       local feedkey = function(key, mode)
         vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
